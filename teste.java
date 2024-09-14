55c3f98d70ecf2a405dbfe5c66bdba8980e91714
Eduardo Tribst <eduardo.tribst@gmail.com>
2024-09-14 16:50:39 -0300

    commit1

--- teste.java
+++ teste.java
@@ -2,6 +2,7 @@ public class teste {
     public static void main(String[] args){
         System.out.print("teste");
         System.out.println("teste");
+        System.out.println("teste");
     }
 
 
